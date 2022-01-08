dd04e1946d04c0eaeda1810f1d6f1aa84643bec3
Iiro Jäppinen <iiro@jappinen.fi>
2022-01-08 19:09:47 +0200

    test: integration tests run the entire lint-staged cli

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,10 @@
 const config = {
   collectCoverage: true,
-  collectCoverageFrom: [
-    'lib/**/*.js',
-    // Avoid ESM import.meta parse error.
-    // (Can't measure coverage anyway, it's always mocked)
-    '!lib/resolveConfig.js',
-  ],
-  moduleDirectories: ['node_modules'],
+  collectCoverageFrom: ['lib/**/*'],
   setupFiles: ['./testSetup.js'],
   snapshotSerializers: ['jest-snapshot-serializer-ansi'],
   testEnvironment: 'node',
-  transform: {
-    '\\.[jt]sx?$': 'babel-jest',
-    '\\.mjs$': 'babel-jest',
-  },
-  /** Also transform ESM packages in `node_modules` */
-  transformIgnorePatterns: [],
+  transform: {},
 }
 
 export default config
